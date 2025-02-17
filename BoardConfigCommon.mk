5130283fde58eaf8208076a915f19a0a9d7b0798
LuK1337 <priv.luk@gmail.com>
2025-02-17 23:07:57 +0100

    sdm845-common: Migrate to avbtool --set_verification_disabled_flag
    Change-Id: Ib73f4d97a923b25e25848b24a3554cdd4de1a5ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -164,7 +164,7 @@ SOONG_CONFIG_ONEPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(COMMON_PATH)/touch/inclu
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_verification_disabled_flag
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
