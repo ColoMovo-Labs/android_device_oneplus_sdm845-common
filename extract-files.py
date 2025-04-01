a6321b7c792aec0b9de4d8fe502e4899b651a1ed
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    sdm845-common: Add libwfdservice_shim_v1 for WFD
    e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I60691f2bab3f52fd9ade36ed6fcf6a2ef13a18c9

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
         .apktool_patch('blob-patches/PowerOffAlarm.patch', '-r'),
+    'system_ext/bin/wfdservice': blob_fixup()
+        .add_needed('libwfdservice_shim_v1.so'),
     'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
